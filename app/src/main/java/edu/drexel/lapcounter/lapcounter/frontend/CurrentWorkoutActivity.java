7137cc04a6a7d2f760b9d862ce71ddf952484b58
simona <simona@simonas-MacBook-Pro.local>
2019-02-11 16:19:53 -0500

    add timer and start and stop buttons to current workout screen

--- app/src/main/java/edu/drexel/lapcounter/lapcounter/frontend/CurrentWorkoutActivity.java
+++ app/src/main/java/edu/drexel/lapcounter/lapcounter/frontend/CurrentWorkoutActivity.java
@@ -4,12 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.widget.Button;
+import android.widget.TextView;
 
 import edu.drexel.lapcounter.lapcounter.R;
 import edu.drexel.lapcounter.lapcounter.frontend.navigationbar.NavBar;
 
 public class CurrentWorkoutActivity extends AppCompatActivity {
     private final NavBar mNavBar = new NavBar(this, R.id.navigation_home);
+
+    private Button startButton;
+    private Button pauseButton;
+    private Button restartButton;
+
+    private TextView timerValue;
+
+    private long startTime = 0L;
+
+    private Handler customHandler = new Handler();
+
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +38,66 @@ public class CurrentWorkoutActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Current Workout");
 
         mNavBar.init();
+
+        timerValue = (TextView) findViewById(R.id.timerValue);
+
+        startButton = (Button) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+
+            }
+        });
+
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                timeSwapBuff += timeInMilliseconds;
+                customHandler.removeCallbacks(updateTimerThread);
+
+            }
+        });
+
+        restartButton = (Button) findViewById(R.id.restartButton);
+
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
+
+
+
+
+        private Runnable updateTimerThread = new Runnable() {
+
+            public void run() {
+
+                timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+
+                updatedTime = timeSwapBuff + timeInMilliseconds;
+
+                int secs = (int) (updatedTime / 1000);
+                int mins = secs / 60;
+                secs = secs % 60;
+                int milliseconds = (int) (updatedTime % 1000);
+                timerValue.setText("" + mins + ":"
+                        + String.format("%02d", secs) + ":"
+                        + String.format("%03d", milliseconds));
+                customHandler.postDelayed(this, 0);
+            }
+
+        };
+
 
     // TODO: Maybe rename WorkoutHistory -> Analytics so it's less confusing?
     public void viewWorkoutHistory(View view) {
