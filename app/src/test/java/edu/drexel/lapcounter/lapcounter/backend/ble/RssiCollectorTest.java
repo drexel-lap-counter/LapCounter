4c385d151b569f03e1678a205ee17a16a279b13f
rkr35 <rkr35@drexel.edu>
2019-05-03 13:28:16 -0400

    Setup unit tests for RssiCollector

---
+++ app/src/test/java/edu/drexel/lapcounter/lapcounter/backend/ble/RssiCollectorTest.java
@@ -0,0 +1,60 @@
+package edu.drexel.lapcounter.lapcounter.backend.ble;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RssiCollectorTest {
+
+    @Test
+    public void collect_does_not_add_element_when_disabled() {
+    }
+
+    @Test
+    public void isEnabled() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void enable() {
+    }
+
+    @Test
+    public void disable() {
+    }
+
+    @Test
+    public void mean() {
+    }
+
+    @Test
+    public void stdDev() {
+    }
+
+    @Test
+    public void median() {
+    }
+
+    @Test
+    public void min() {
+    }
+
+    @Test
+    public void max() {
+    }
+
+//    @Test
+//    public void toString() {
+//    }
+//
+//    @Test
+//    public void toString1() {
+//    }
+
+    @Test
+    public void computeThreshold() {
+    }
+}
