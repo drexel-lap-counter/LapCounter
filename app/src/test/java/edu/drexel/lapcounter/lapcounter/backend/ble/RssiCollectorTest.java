7dadcb0993a0de347f9b09056af61a1e6044dd3a
rkr35 <rkr35@drexel.edu>
2019-05-03 13:52:34 -0400

    Add unit tests for RssiCollector::{min, max, clear}

--- app/src/test/java/edu/drexel/lapcounter/lapcounter/backend/ble/RssiCollectorTest.java
+++ app/src/test/java/edu/drexel/lapcounter/lapcounter/backend/ble/RssiCollectorTest.java
@@ -26,6 +26,41 @@ public class RssiCollectorTest {
     }
 
     @Test
+    public void collect_updates_min_and_max_of_one_element() {
+        RssiCollector rc = new RssiCollector();
+        rc.enable();
+
+        int element = 54;
+        rc.collect(element);
+
+        assertEquals(element, rc.min());
+        assertEquals(element, rc.max());
+    }
+
+    @Test
+    public void collect_updates_min_and_max() {
+        RssiCollector rc = new RssiCollector();
+        rc.enable();
+
+        int element = 54;
+        rc.collect(element);
+
+        assertEquals(element, rc.min());
+        assertEquals(element, rc.max());
+
+        int min = 32;
+        rc.collect(min);
+        assertEquals(min, rc.min());
+        assertEquals(element, rc.max());
+
+        int max = 76;
+        rc.collect(max);
+        assertEquals(min, rc.min());
+        assertEquals(max, rc.max());
+    }
+
+
+    @Test
     public void isEnabled_false_when_disabled() {
         assertFalse(new RssiCollector().isEnabled());
     }
@@ -47,15 +82,51 @@ public class RssiCollectorTest {
     }
 
     @Test
-    public void clear() {
+    public void clear_empties_buffers() {
+        RssiCollector rc = new RssiCollector();
+        rc.enable();
+        rc.collect(54);
+        rc.collect(73);
+        rc.clear();
+        RssiCollector.ListSizes ls = rc.getListSizes();
+        assertEquals(0, ls.NumValues);
+        assertEquals(0, ls.NumDeltas);
+    }
+
+
+    @Test
+    public void clear_resets_min_and_max() {
+        RssiCollector rc = new RssiCollector();
+        rc.enable();
+
+        int min = 54;
+        int max = 73;
+
+        rc.collect(min);
+        rc.collect(max);
+
+        assertEquals(min, rc.min());
+        assertEquals(max, rc.max());
+
+        rc.clear();
+
+        assertEquals(Integer.MAX_VALUE, rc.min());
+        assertEquals(Integer.MIN_VALUE, rc.max());
     }
 
     @Test
-    public void enable() {
-    }
-
-    @Test
-    public void disable() {
+    public void clear_can_still_add_elements_afterwards() {
+        RssiCollector rc = new RssiCollector();
+        rc.enable();
+        rc.collect(54);
+        rc.collect(73);
+        rc.clear();
+        rc.collect(63);
+        rc.collect(102);
+        rc.collect(85);
+        RssiCollector.ListSizes ls = rc.getListSizes();
+        assertEquals(3, ls.NumValues);
+        assertEquals(3, ls.NumDeltas);
     }
 
     @Test
