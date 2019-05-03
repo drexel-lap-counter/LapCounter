0fadb7409f16eff3abf7facafa10ca276b3e3ca3
rkr35 <rkr35@drexel.edu>
2019-05-03 12:57:46 -0400

    Add unit tests for MovingAverage::filter(double)

--- app/src/test/java/edu/drexel/lapcounter/lapcounter/backend/ble/MovingAverageTest.java
+++ app/src/test/java/edu/drexel/lapcounter/lapcounter/backend/ble/MovingAverageTest.java
@@ -1,5 +1,7 @@
 package edu.drexel.lapcounter.lapcounter.backend.ble;
 
+import android.util.CustomAssertions;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -33,11 +35,62 @@ public class MovingAverageTest {
         assertFalse(ma.windowIsFull());
     }
 
+    @Test
+    public void filter_returns_element_when_buffer_has_one_element() {
+        MovingAverage ma = new MovingAverage(10);
+        double value = 84;
+        CustomAssertions.assertEquals(ma.filter(value), value);
+    }
 
     @Test
-    public void filter() {
+    public void filter_returns_midpoint_when_buffer_has_two_elements() {
+        double value1 = 50;
+        double value2 = 100;
 
+        MovingAverage ma = new MovingAverage(10);
+        ma.filter(value1);
+
+        CustomAssertions.assertEquals(ma.filter(value2), (value1 + value2) / 2);
     }
+
+    @Test
+    public void filter_returns_average_up_to_and_including_full_buffer() {
+        final int size = 100;
+        MovingAverage ma = new MovingAverage(size);
+
+        for (int i = 1; i <= size; ++i) {
+            CustomAssertions.assertEquals(ma.filter(i), (i + 1) / 2.0);
+        }
+    }
+
+    @Test
+    public void filter_returns_average_after_adding_one_element_to_full_buffer() {
+        final int size = 100;
+        MovingAverage ma = new MovingAverage(size);
+
+        for (int i = 1; i <= size; ++i) {
+            ma.filter(i);
+        }
+
+        CustomAssertions.assertEquals(ma.filter(size + 1), 1 + (size + 1) / 2.0);
+    }
+
+    @Test
+    public void filter_returns_average_after_adding_k_elements_to_full_buffer() {
+        final int size = 100;
+        final int k = 50;
+
+        MovingAverage ma = new MovingAverage(size);
+
+        for (int i = 1; i <= size; ++i) {
+            ma.filter(i);
+        }
+
+        for (int i = 1; i <= k; ++i) {
+            CustomAssertions.assertEquals(ma.filter(size + i), i + (size + 1) / 2.0);
+        }
+    }
+
 
     @Test
     public void clear() {
