27c49421b5342e3a755066220024ecc69a624e1e
rkr35 <rkr35@drexel.edu>
2019-05-03 12:32:45 -0400

    Refactor custom assertEquals for doubles into separate util class

---
+++ app/src/test/java/android/util/CustomAssertions.java
@@ -0,0 +1,31 @@
+package android.util;
+
+import static org.junit.Assert.assertTrue;
+
+public class CustomAssertions {
+    //https://floating-point-gui.de/errors/comparison/
+    public static boolean nearlyEqual(double a, double b, double epsilon) {
+        if (a == b) {
+            // shortcut, handles infinities
+            return true;
+        }
+
+        final double diff = Math.abs(a - b);
+
+        if (a == 0 || b == 0 || diff < Double.MIN_NORMAL) {
+            // a or b is zero or both are extremely close to it
+            // relative error is less meaningful here
+            return diff < (epsilon * Double.MIN_NORMAL);
+        }
+
+        final double absA = Math.abs(a);
+        final double absB = Math.abs(b);
+
+        // use relative error
+        return diff / Math.min((absA + absB), Double.MAX_VALUE) < epsilon;
+    }
+
+    public static void assertEquals(double a, double b) {
+        assertTrue(nearlyEqual(a, b, 10 * Double.MIN_VALUE));
+    }
+}
