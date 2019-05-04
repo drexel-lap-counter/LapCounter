2adaf05be1b3ad9cb1f437c28d34187e73a50bbf
rkr35 <rkr35@drexel.edu>
2019-05-04 12:25:48 -0400

    Setup unit tests for RSSIManager

---
+++ app/src/test/java/edu/drexel/lapcounter/lapcounter/backend/ble/RSSIManagerTest.java
@@ -0,0 +1,40 @@
+package edu.drexel.lapcounter.lapcounter.backend.ble;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RSSIManagerTest {
+
+    @Test
+    public void scheduleRssiRequest() {
+    }
+
+    @Test
+    public void initCallbacks() {
+    }
+
+    @Test
+    public void getRssi() {
+    }
+
+    @Test
+    public void getDirection() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void stopRssiRequests() {
+    }
+
+    @Test
+    public void setDeltasSize() {
+    }
+
+    @Test
+    public void setFilterSize() {
+    }
+}
